67f5b85506e83d8e2b0cfeaf74461b7a39e81145
Will Carpenter <willcarpenterwastaken@gmail.com>
2024-08-27 15:54:27 -0500

    resizing

--- js/input.js
+++ js/input.js
@@ -1,62 +1,56 @@
-(function() {
-    var pressedKeys = {};
+(function () {
+  var pressedKeys = {};
 
-    function setKey(event, status) {
-        var code = event.keyCode;
-        console.log(code);
-        var key;
+  function setKey(event, status) {
+    var code = event.keyCode;
+    console.log(code);
+    var key;
 
-        switch (code) {
-            case 188:
-                key = "SPACE";
-                break;
-            case 37:
-                key = "LEFT";
-                break;
-            case 53:
-                key = "UP";
-                break;
-            case 39:
-                key = "RIGHT";
-                break;
-            case 40:
-                key = "DOWN";
-                break;
-            case 38:
-                key = "JUMP";
-                break;
-            case 32:
-                key = "RUN";
-                break;
-            default:
-                key = String.fromCharCode(code);
-        }
-
-        pressedKeys[key] = status;
+    switch (code) {
+      case 37:
+        key = "LEFT";
+        break;
+      case 39:
+        key = "RIGHT";
+        break;
+      case 40:
+        key = "DOWN";
+        break;
+      case 38:
+        key = "JUMP";
+        break;
+      case 32:
+        key = "RUN";
+        break;
+      default:
+        key = String.fromCharCode(code);
     }
 
-    document.addEventListener("keydown", function(e) {
-        setKey(e, true);
-    });
+    pressedKeys[key] = status;
+  }
 
-    document.addEventListener("keyup", function(e) {
-        setKey(e, false);
-    });
+  document.addEventListener("keydown", function (e) {
+    setKey(e, true);
+  });
 
-    window.addEventListener("blur", function() {
-        pressedKeys = {};
-    });
+  document.addEventListener("keyup", function (e) {
+    setKey(e, false);
+  });
 
-    window.input = {
-        isDown: function(key) {
-            return pressedKeys[key.toUpperCase()];
-        },
-        reset: function() {
-            pressedKeys["RUN"] = false;
-            pressedKeys["LEFT"] = false;
-            pressedKeys["RIGHT"] = false;
-            pressedKeys["DOWN"] = false;
-            pressedKeys["JUMP"] = false;
-        },
-    };
+  window.addEventListener("blur", function () {
+    pressedKeys = {};
+  });
+
+  window.input = {
+    isDown: function (key) {
+      return pressedKeys[key.toUpperCase()];
+    },
+    reset: function () {
+      pressedKeys["RUN"] = false;
+      pressedKeys["LEFT"] = false;
+      pressedKeys["RIGHT"] = false;
+      pressedKeys["DOWN"] = false;
+      pressedKeys["JUMP"] = false;
+    },
+  };
 })();
